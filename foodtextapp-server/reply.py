c2775c0ca13c4b27b3d7d6245c4751ad27cab0dd
antonyshiu <35156281+antonyshiu@users.noreply.github.com>
2021-01-10 08:16:57 -0800

    added a reply file

---
+++ foodtextapp-server/reply.py
@@ -0,0 +1,41 @@
+from flask import Flask, request
+from twilio import twiml
+import main
+
+app = Flask(__name__)
+
+@app.route('/sms', methods=['POST'])  #fix this?
+def sms:
+    number = request.form['From']
+    message_body = request.form['Body']
+
+    if message_body == 'Y' or message_body == 'y':
+        
+
+    return None
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+
+
+##from flask import Flask, request, redirect
+##from twilio.twiml.messaging_response import MessagingResponse
+
+##app = Flask(__name__)
+##
+##@app.route("/sms", methods=['GET', 'POST'])
+##def sms_reply():
+##    """Respond to incoming calls with a simple text message."""
+##    # Start our TwiML response
+##    resp = MessagingResponse()
+##
+##    # Add a message
+##    resp.message("The Robots are coming! Head for the hills!")
+##
+##    return str(resp)
+##
+##if __name__ == "__main__":
+##    app.run(debug=True)
