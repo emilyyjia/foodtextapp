4ba5340d27c66e28eaea7b26c324d658744102ba
Anna Song <annajiwons@gmail.com>
2021-01-09 20:18:52 -0800

    add date range picker

--- foodtextapp-client/src/App.js
+++ foodtextapp-client/src/App.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-daterangepicker/daterangepicker.css';
 import React from 'react';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
--- foodtextapp-client/src/components/GiveFood.js
+++ foodtextapp-client/src/components/GiveFood.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import Button from 'react-bootstrap/Button'; 
+import Button from 'react-bootstrap/Button';
+import DateRangePicker from 'react-bootstrap-daterangepicker';
 import Form from 'react-bootstrap/Form';
 
 const CenterContainer = styled.div`
@@ -46,7 +47,14 @@ const GiveFood = () => {
                         </StyledLabel>
                         <Form.Control as='textarea'></Form.Control>
                     </Form.Group>
-                    
+                    <Form.Group controlId="form.date">
+                        <StyledLabel>
+                            Pick-up Date and Time
+                        </StyledLabel>
+                        <DateRangePicker initialSettings={{ startDate: '1/1/2014', endDate: '3/1/2014' }}>
+                            <Form.Control></Form.Control>
+                        </DateRangePicker>
+                    </Form.Group>
                 </StyledForm>
         </CenterContainer>
     );
